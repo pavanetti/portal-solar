c4f69eddefba92b5d599b238dc2dc3fafb6ebd5a
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-09-19 21:38:02 -0300

    Add test to frontend code

---
+++ test/javascript/freight.test.js
@@ -0,0 +1,7 @@
+const { freightCostByCep } = require('freight');
+
+describe('Freight computation', () => {
+  it('throws an exception if receives an invalid CEP', () => {
+    expect(freightCostByCep('11000xxx')).rejects.toThrow()
+  })
+})
