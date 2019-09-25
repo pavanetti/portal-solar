bbe2f0c5eb5bb622d79efa0e672785018274f661
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-09-24 22:18:07 -0300

    Fix js tests

--- test/javascript/freight.test.js
+++ test/javascript/freight.test.js
@@ -2,6 +2,6 @@ const { freightCostByCep } = require('freight');
 
 describe('Freight computation', () => {
   it('throws an exception if receives an invalid CEP', () => {
-    expect(freightCostByCep('11000xxx')).rejects.toThrow()
+    expect(freightCostByCep('11000xxx')).rejects.toEqual('CEP inválido')
   })
 })
